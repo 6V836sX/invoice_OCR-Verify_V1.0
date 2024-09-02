d42d4d44daffbb916d74dd16523455152c8eecb8
6V836sX <61952024+6V836sX@users.noreply.github.com>
2024-09-02 14:10:12 +0800

    invoice

--- invoice_OCR.py
+++ invoice_OCR.py
@@ -1,1 +1,218 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*-# @Time    : 2023/12/27 22:15# @Author  : summer# @Site    :# @File    : rename_fapiao.py# @Software: PyCharm'''1.pdf转PIL2.PIL 进百度API 识别，输出文件名3.重命名pdf'''import base64import globimport osimport os.pathimport reimport shutilimport tempfileimport pandas as pdimport requestsfrom PIL import Imagefrom pdf2image import convert_from_pathfrom decouple import Config,RepositoryEnvdef pdf2jpg(filename, outputDir):    with tempfile.TemporaryDirectory() as path:        images = convert_from_path(pdf_path=filename, dpi=200, output_folder=None, last_page=1,                                   first_page=None,                                   fmt='jpg')        for index, img in enumerate(images):            (path, filename) = os.path.split(filename)            (file, ext) = os.path.splitext(filename)            output_path = os.path.join(outputDir, file + '_page_%s.jpg' % (index))            img.save(output_path)    return output_pathdef convertjpg(jpgfile,outdir,width=1920,height=1215):    img=Image.open(jpgfile)    try:        new_img=img.resize((width,height),Image.BILINEAR)        new_img.save(os.path.join(outdir,os.path.basename(jpgfile)))    except Exception as e:        print(e)    return os.path.join(outdir,os.path.basename(jpgfile))def get_access_token(AK, SK):    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' + AK + '&client_secret=' + SK    response = requests.get(host)    #     if response:    #         print(response.json())    return response.json().get('access_token')def renew_keys():    configuration = Config(repository = RepositoryEnv('config.env'))    AK = configuration.get('AK')    SK = configuration.get('SK')    access_token = get_access_token(AK, SK)  # access_token 从上面模块中定义的函数实时提取    print('access_token更新成功：' + access_token)    return access_tokendef OCR_vat(jpgpath, access_token):    f = open(jpgpath, 'rb')    img = base64.b64encode(f.read())    params = {"image": img}    request_url_0 = "https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice"  #电子发票识别url    request_url = request_url_0 + "?access_token=" + access_token    headers = {'content-type': 'application/x-www-form-urlencoded'}    j = requests.post(request_url, data=params, headers=headers)    if j:        return j.json().get('words_result')    else:        return (print('识别失败',jpgpath))def parse_data(json_data):    try:        data={            "InvoiceCode":json_data.get("InvoiceCode"),            "InvoiceNum":json_data.get("InvoiceNum")            }        file_name = data.get("InvoiceCode") + '_' + data.get("InvoiceNum")        return str(file_name)    except Exception as e:        print(e)        return print(f'解析失败:{json_data}')def rename_pdf(rsc,oldname,dst,filename):    os.rename(rsc,os.path.join(dst,filename))    # print('{}--->{}'.format(oldname, filename))    return Nonedef parse_content(json_data):    try:        data={            "InvoiceType":str(json_data.get("InvoiceType")),            "InvoiceCode":str(json_data.get("InvoiceCode")),            "InvoiceNum":str(json_data.get("InvoiceNum")),            "InvoiceDate": str(json_data.get("InvoiceDate")),            "CheckCode": str(json_data.get("CheckCode")),            "PurchaserName": str(json_data.get("PurchaserName")),            "PurchaserRegisterNum": json_data.get("PurchaserRegisterNum"),            "SellerName": json_data.get("SellerName"),            "SellerRegisterNum": json_data.get("SellerRegisterNum"),            "TotalAmount": float(json_data.get("TotalAmount")),#不含税金额            "AmountInFiguers": float(json_data.get("AmountInFiguers")),#价税合计            "Remarks": json_data.get("Remarks")            }        df = pd.DataFrame( data = data, index ={'序号'})        return df    except Exception as e:        print(e)        print(f'解析失败:{json_data}')        return None #如果解析失败，返回一个空的dataframe#写一个函数，正则匹配获取InvoiceDate的月份，然后转换成数字def get_month(InvoiceDate):    try:        month=re.findall(r'\d+',InvoiceDate)[1]        return str(month)    except Exception as e:        print(e)        return str(InvoiceDate)def get_year(InvoiceDate):    try:        year=re.findall(r'\d+',InvoiceDate)[0]        return str(year)    except Exception as e:        print(e)        return str(InvoiceDate)if __name__ == "__main__":    step=0 #标记进度    targetdir: str=input('Enter the target dir:')  #输入目标文件夹    #遍历targetir目录下的所有pdf文件    rscfiles=glob.glob(targetdir+'/**/*.[pP][dD][fF]',recursive=True) #获取目标文件夹下的所有pdf文件    print(f'共有{len(rscfiles)}个pdf文件待处理')    outdir=os.path.join("./")+"/temp"    if not os.path.exists(outdir):        os.makedirs(outdir)    access_token=renew_keys()    df_sum=[]    for rscfile in rscfiles:        print(rscfile)        jpgfile = pdf2jpg(rscfile, outputDir=outdir)        convertjpg(jpgfile=jpgfile, outdir=outdir)        json_data = OCR_vat(jpgpath=jpgfile, access_token=access_token)        file_name = str(parse_data(json_data)) + '.pdf'        #如果targetdir目录下不存在名为PurchaserName的文件夹，则创建一个。        if not os.path.exists(os.path.join(targetdir,str(json_data.get("PurchaserName")))):            os.makedirs(os.path.join(targetdir,str(json_data.get("PurchaserName"))))        # 如果PurchaserName目录下不存在对应月份名称的文件夹，则创建一个。        if not os.path.exists(os.path.join(targetdir,str(json_data.get("PurchaserName")),get_year(json_data.get( "InvoiceDate" )),get_month(json_data.get( "InvoiceDate" )))):            os.makedirs(os.path.join(targetdir,str(json_data.get("PurchaserName")),get_year(json_data.get( "InvoiceDate" )),get_month(json_data.get( "InvoiceDate" ))))        dst=os.path.join(targetdir,str(json_data.get("PurchaserName")),get_year(json_data.get( "InvoiceDate" )),get_month(json_data.get( "InvoiceDate" )))        rename_pdf(rsc=rscfile, oldname=rscfile.split('/')[-1], dst=dst, filename=file_name)        step+=1        print(f'已完成{step}/{len(rscfiles)}个文件的处理')        df_sum.append(parse_content(json_data))    result =pd.concat(df_sum).sort_values('InvoiceDate', ascending=True)    #合并dataframe，并按照InvoiceDate排序    result.drop_duplicates(inplace=True)    #去重    result.index = pd.Index(range(1,len(result)+1),name = "序号")    result.to_excel(excel_writer=str(targetdir+"/sum.xlsx"))    shutil.rmtree(outdir)    print(f'finished with {len(rscfiles)} invoice files')
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/27 22:15
+# @Author  : summer
+# @Site    :
+# @File    : rename_fapiao.py
+# @Software: PyCharm
+'''
+1.pdf转PIL
+2.PIL 进百度API 识别，输出文件名
+3.重命名pdf
+
+'''
+import base64
+import glob
+import os
+import os.path
+import re
+import shutil
+import tempfile
+import pandas as pd
+import requests
+from PIL import Image
+from decouple import Config,RepositoryEnv
+from pdf2image import convert_from_path
+from tqdm import tqdm
+import logging
+import os
+from datetime import datetime
+
+def setup_logging(target_dir):
+    # 获取当前日期和时间的时间戳
+    current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    
+    # 提取目标目录的名称
+    dir_name = os.path.basename(target_dir)
+    
+    # 创建日志文件名
+    log_filename = f"./OCRLog/{current_timestamp}_{dir_name}.log"
+    
+    # 设置日志
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    # 创建文件处理器
+    file_handler = logging.FileHandler(log_filename, mode='w', encoding='utf-8')
+    file_handler.setLevel(logging.INFO)
+    
+    # 创建控制台处理器
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    
+    # 设置格式器
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    
+    # 获取根日志记录器并添加处理器
+    root_logger = logging.getLogger()
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
+    
+    logging.info(f"开始处理目录: {target_dir}")
+
+def pdf2jpg(filename, outputDir):
+    with tempfile.TemporaryDirectory() as path:
+        images = convert_from_path(pdf_path=filename, dpi=200, output_folder=None, last_page=1,
+                                   first_page=None,
+                                   fmt='jpg')
+        for index, img in enumerate(images):
+            (path, filename) = os.path.split(filename)
+            (file, ext) = os.path.splitext(filename)
+            output_path = os.path.join(outputDir, file + '_page_%s.jpg' % (index))
+            img.save(output_path)
+    return output_path
+
+
+def convertjpg(jpgfile,outdir,width=1920,height=1215):
+    img=Image.open(jpgfile)
+    try:
+        new_img=img.resize((width,height),Image.BILINEAR)
+        new_img.save(os.path.join(outdir,os.path.basename(jpgfile)))
+    except Exception as e:
+        print(e)
+    return os.path.join(outdir,os.path.basename(jpgfile))
+
+def get_access_token(AK, SK):
+    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' + AK + '&client_secret=' + SK
+    response = requests.get(host)
+    #     if response:
+    #         print(response.json())
+    # print(type(response.json().get('access_token')))
+    return response.json().get('access_token')
+
+
+
+def renew_keys():
+    configuration = Config(repository = RepositoryEnv('config.env'))
+    AK = configuration.get('AK')
+    SK = configuration.get('SK')
+    access_token = get_access_token(AK, SK)  # access_token 从上面模块中定义的函数实时提取
+    print('access_token更新成功：' + access_token)
+    return access_token
+
+
+def OCR_vat(jpgpath, access_token):
+    f = open(jpgpath, 'rb')
+    img = base64.b64encode(f.read())
+    params = {"image": img}
+    request_url_0 = "https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice"  #电子发票识别url
+    request_url = request_url_0 + "?access_token=" + access_token
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    j = requests.post(request_url, data=params, headers=headers)
+    if j:
+        return j.json().get('words_result')
+    else:
+        return (print('识别失败',jpgpath))
+
+def parse_data(json_data):
+    try:
+        data={
+            "InvoiceCode":json_data.get("InvoiceCode"),
+            "InvoiceNum":json_data.get("InvoiceNum")
+            }
+        file_name = data.get("InvoiceCode") + '_' + data.get("InvoiceNum")
+        return str(file_name)
+    except Exception as e:
+        print(e)
+        return print(f'解析失败:{json_data}')
+
+def rename_pdf(rsc,oldname,dst,filename):
+    os.rename(rsc,os.path.join(dst,filename))
+    # print('{}--->{}'.format(oldname, filename))
+    return None
+
+
+def parse_content(json_data):
+    try:
+        data={
+            "InvoiceType":str(json_data.get("InvoiceType")),
+            "InvoiceCode":str(json_data.get("InvoiceCode")),
+            "InvoiceNum":str(json_data.get("InvoiceNum")),
+            "InvoiceDate": str(json_data.get("InvoiceDate")),
+            "CheckCode": str(json_data.get("CheckCode")),
+            "PurchaserName": str(json_data.get("PurchaserName")),
+            "PurchaserRegisterNum": json_data.get("PurchaserRegisterNum"),
+            "SellerName": json_data.get("SellerName"),
+            "SellerRegisterNum": json_data.get("SellerRegisterNum"),
+            "TotalAmount": float('{:.2f}'.format(float(json_data.get("TotalAmount")))),#不含税金额
+            "AmountInFiguers": float('{:.2f}'.format(float(json_data.get("AmountInFiguers")))),#价税合计
+            "Remarks": json_data.get("Remarks")
+            }
+        return pd.DataFrame(data=data, index=[0])
+    except Exception as e:
+        logging.error(f'解析失败: {e}')
+        logging.error(f'JSON数据: {json_data}')
+        return pd.DataFrame()  # 如果解析失败，返回一个空的dataframe
+
+#写一个函数，正则匹配获取InvoiceDate的月份，然后转换成数字
+def get_month(InvoiceDate):
+    try:
+        month=re.findall(r'\d+',InvoiceDate)[1]
+        return str(month)
+    except Exception as e:
+        print(e)
+        return str(InvoiceDate)
+
+def get_year(InvoiceDate):
+    try:
+        year=re.findall(r'\d+',InvoiceDate)[0]
+        return str(year)
+    except Exception as e:
+        print(e)
+        return str(InvoiceDate)
+
+
+def main():
+    targetdir: str = input('Enter the target dir:')  # 输入目标文件夹
+    setup_logging(targetdir)
+    
+    # 遍历targetir目录下的所有pdf文件
+    rscfiles = glob.glob(targetdir + '/**/*.[pP][dD][fF]',recursive = True)  # 获取目标文件夹下的所有pdf文件
+    print(f'共有{len(rscfiles)}个pdf文件待处理')
+
+    outdir = os.path.join("./") + "/temp"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    access_token = renew_keys()
+    df_sum = []
+    for rscfile in tqdm(rscfiles, desc='Processing'):
+        logging.info(f'正在处理文件: {rscfile}')
+        print(rscfile)
+        jpgfile = pdf2jpg(rscfile,outputDir = outdir)
+        convertjpg(jpgfile = jpgfile,outdir = outdir)
+        json_data = OCR_vat(jpgpath = jpgfile,access_token = access_token)
+        df_sum.append(parse_content(json_data))
+
+    # 统计所有发票信息
+    result = pd.concat(df_sum).sort_values(['PurchaserRegisterNum', 'InvoiceDate'], ascending=[True, True])
+    result.drop_duplicates(inplace=True)  # 去重
+    result.index = pd.Index(range(1, len(result) + 1), name="序号")
+    
+    # 输出到Excel文件
+    excel_path = os.path.join(targetdir, "sum.xlsx")
+    result.to_excel(excel_writer=excel_path)
+    
+    logging.info(f'处理完成,共处理了{len(rscfiles)}个发票文件')
+    logging.info(f'发票统计信息已保存到: {excel_path}')
+
+    return None
+
+
+
+if __name__ == "__main__":
+    main()
+
--- invoice_Verify.py
+++ invoice_Verify.py
@@ -2,18 +2,57 @@
 https://cloud.baidu.com/doc/OCR/s/cklbnrnwe #百度官方文档
 
 '''
-
+import os.path
+import logging
+from datetime import datetime
 import requests
 from decouple import Config,RepositoryEnv
 import pandas as pd
 from pprint import pprint
 
 
-def get_access_token(AK,SK):
+def setup_logging(target_dir):
+    # 获取当前日期和时间的时间戳
+    current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    
+    # 提取目标目录的名称
+    dir_name = os.path.basename(target_dir)
+    
+    # 创建日志文件名
+    log_filename = f"./verifyLog/{current_timestamp}_{dir_name}.log"
+    
+    # 设置日志
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    # 创建文件处理器
+    file_handler = logging.FileHandler(log_filename, mode='w', encoding='utf-8')
+    file_handler.setLevel(logging.INFO)
+    
+    # 创建控制台处理器
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    
+    # 设置格式器
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    
+    # 获取根日志记录器并添加处理器
+    root_logger = logging.getLogger()
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
+    
+    logging.info(f"开始处理目录: {target_dir}")
+
+
+def get_access_token(AK, SK):
     host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' + AK + '&client_secret=' + SK
     response = requests.get(host)
     #     if response:
     #         print(response.json())
+    logging.info("获取访问令牌成功")
     return response.json().get('access_token')
 
 
@@ -22,7 +61,7 @@ def renew_keys():
     AK = configuration.get('AK_check')
     SK = configuration.get('SK_check')
     access_token = get_access_token(AK,SK)  # access_token 从上面模块中定义的函数实时提取
-    print('access_token更新成功：' + access_token)
+    logging.info(f"access_token更新成功：{access_token}")
     return access_token
 
 
@@ -55,8 +94,8 @@ def convert_key(key):
         '增值税普通发票':'normal_invoice',
         '增值税普通发票（电子）':'elec_invoice_normal',
         '电子普通发票':'elec_normal_invoice',
-        '电子发票(普通发票)':'elec_invoice_normal',   #全电发票
-        '通行费增值税电子普通发票':'toll_elec_normal_invoice',
+        '电子发票(普通发票)':'elec_invoice_normal',  # 全电发票
+        '通行费电子普票':'toll_elec_normal_invoice',
 
         '增值税普通发票（卷式）':'roll_normal_invoice',
         '区块链电子发票（目前仅支持深圳地区）':'blockchain_invoice',
@@ -68,8 +107,8 @@ def convert_key(key):
     return key_dict[key]
 
 
-def convert_date_str(date_str)->str:
-# 去掉字符串中的非数字字符，只保留数字
+def convert_date_str(date_str) -> str:
+    # 去掉字符串中的非数字字符，只保留数字
     digits = ''.join(filter(str.isdigit,date_str))
 
     # 使用字符串的zfill方法在左侧补足0，确保总长度为8位
@@ -79,9 +118,9 @@ def convert_date_str(date_str)->str:
 
 # 写一个函数，按下列规则转换total_amount 或 AmountInFiguers
 
-def convert_total_amount(InvoiceType,total_amount,AmountInFiguers)->str:
+def convert_total_amount(InvoiceType,total_amount,AmountInFiguers) -> str:
     '''
-    增值税专票、电子专票、区块链电子发票、机动车销售发票、货运专票填写不含税金额；
+    ���值税专票、电子专票、区块链电子发票、机动车销售发票、货运专票填写不含税金额；
     二手车销售发票填写车价合计；
     全电发票（专用发票）、全电发票（普通发票）填写价税合计金额，其他类型发票可为空
     '''
@@ -95,58 +134,68 @@ def convert_total_amount(InvoiceType,total_amount,AmountInFiguers)->str:
     else:
         return None
 
+
 '''
 OCR-增值税发票验真
 '''
 
 
-def fp_check(payload,access_token)->str:
+def fp_check(payload, access_token) -> str:
     request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice_verification"
     request_url = request_url + "?access_token=" + access_token
     headers = {'content-type':'application/x-www-form-urlencoded'}
     response = requests.post(request_url,data = payload,headers = headers)
     if response:
-        # print(response.json())
-        pprint(f"验真结果:{str(response.json().get('VerifyMessage'))}")
-    return str(response.json().get('VerifyMessage'))
-
+        verify_message = str(response.json().get('VerifyMessage'))
+        logging.info(f"验真结果: {verify_message}")
+    return verify_message
 
 
 if __name__ == '__main__':
-    access_token = renew_keys() # access_token 从上面模块中定义的函数实时提取,使用.env中的AK_check和SK_check
-    # 读取本目录下sum.xlsx文件，从中获取fp_check所需要的参数
-    rscfilepath =input('请输入rename后的文件完整路径：')
-    df = pd.read_excel(input=rscfilepath,sheet_name = 'Sheet1',dtype = str)
-    df['check_result'] = ['']*df.shape[0]
-    for index,row in df.iterrows():
-        # print('这是第{}行'.format(index))
+    rscfilepath = input('��输入sum.xlsx文件完整路径：')
+    setup_logging(os.path.dirname(rscfilepath))
+    
+    access_token = renew_keys()
+    logging.info("开始读取Excel文件")
+    df = pd.read_excel(io=rscfilepath, sheet_name='Sheet1', dtype=str)
+    df['check_result'] = [''] * df.shape[0]
+    
+    for index, row in df.iterrows():
+        logging.info(f"处理第 {index+1} 行")
         # 将row转换为字典
         params = row.to_dict()
+        logging.info(f"当前行数据: {params}")
+        
         # 用params构造fp_check所需要的参数,全电发票参数不同。
-        if convert_key(params["InvoiceType"]) in ['elec_invoice_special','elec_invoice_normal']:
+        invoice_type = convert_key(params["InvoiceType"])
+        logging.info(f"转换后的发票类型: {invoice_type}")
+        
+        if invoice_type in ['elec_invoice_special','elec_invoice_normal']:
             params1 = {
-                           "invoice_num":str(params["InvoiceNum"]),
-                           "invoice_date":convert_date_str(str(params["InvoiceDate"])),
-                           "invoice_type":convert_key(params["InvoiceType"]),
-                           "total_amount":str(convert_total_amount(convert_key(params["InvoiceType"]),params["TotalAmount"],
-                                                                   params["AmountInFiguers"]))
-                           }
+                "invoice_num":str(params["InvoiceNum"]),
+                "invoice_date":convert_date_str(str(params["InvoiceDate"])),
+                "invoice_type":convert_key(params["InvoiceType"]),
+                "total_amount":str(convert_total_amount(convert_key(params["InvoiceType"]),params["TotalAmount"],
+                                                        params["AmountInFiguers"]))
+            }
             payload = f'invoice_code=&invoice_num={params1["invoice_num"]}&invoice_date={params1["invoice_date"]}&invoice_type={params1["invoice_type"]}&check_code=&total_amount={params1["total_amount"]}'
         else:
             params1 = {
-                       "invoice_code":str(params["InvoiceCode"]),
-                       "invoice_num":str(params["InvoiceNum"]),
-                       "invoice_date":convert_date_str(str(params["InvoiceDate"])),
-                       "invoice_type":convert_key(params["InvoiceType"]),
-                       "check_code":str(params["CheckCode"])[-6:],
-                       "total_amount":str(convert_total_amount(convert_key(params["InvoiceType"]),params["TotalAmount"],params["AmountInFiguers"]))
-                       }
+                "invoice_code":str(params["InvoiceCode"]),
+                "invoice_num":str(params["InvoiceNum"]),
+                "invoice_date":convert_date_str(str(params["InvoiceDate"])),
+                "invoice_type":convert_key(params["InvoiceType"]),
+                "check_code":str(params["CheckCode"])[-6:],
+                "total_amount":str(convert_total_amount(convert_key(params["InvoiceType"]),params["TotalAmount"],
+                                                        params["AmountInFiguers"]))
+            }
             payload = f'invoice_code={params1["invoice_code"]}&invoice_num={params1["invoice_num"]}&invoice_date={params1["invoice_date"]}&invoice_type={params1["invoice_type"]}&check_code={params1["check_code"]}&total_amount={params1["total_amount"]}'
 
-
-        print(f'开始验真：{payload}\n\n')
-        #将验真结果写入df
-        df.loc[index,'check_result']=fp_check(payload,access_token)
-        df.setsort_values(by = index,ascending = False,inplace = True)
-        df.to_excel(excel_writer = "checked.xlsx")
-        print('验真结束：','\n\n','-+-+-' * 20)
+        logging.info(f'开始验真：{payload}')
+        # 将验真结果写入df
+        df.loc[index,'check_result'] = fp_check(payload,access_token)
+        # df.sort_values(by = index,ascending = False,inplace = True)
+        check_result_path= os.path.join(os.path.dirname(rscfilepath) ,'_checked.xlsx')
+        df.to_excel(excel_writer = check_result_path)
+        logging.info(f'第 {index+1} 行验真结束')
+        logging.info("所有处理完成")
